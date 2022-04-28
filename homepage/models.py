2af45ce6a6f463ac36ef2c38de6bd874e3024683
silverania <spaceissimo@gmail.com>
2022-04-28 15:20:03 +0200

    uslug_screa_da_solo_nelreceveirinmodel_eventsave

--- homepage/models.py
+++ homepage/models.py
@@ -6,84 +6,102 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from .models import User
 from user.models import Profile
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 #custom modelmanager classe per visualizzare tutorial  in admin
+
+
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super(PublishedManager,
-        self).get_queryset().filter(status='pubblicato')
+                     self).get_queryset().filter(status='pubblicato')
 
 #custo modelmanager classe per visualizzare tutorial bozza in admin
+
+
 class BozzaManager(models.Manager):
     def get_queryset(self):
         return super(BozzaManager,
-        self).get_queryset().filter(status='bozza')
+                     self).get_queryset().filter(status='bozza')
+
 
 class Visite(models.Model):
-    visite=models.PositiveIntegerField(default=1)
+    visite = models.PositiveIntegerField(default=1)
+
     def __str__(self):
         return "%s" % (self.visite)
 
+
 class Category(models.Model):
     CATEGORY = (
-    ('linux', 'Linux'),
-    ('web', 'Web'),
-    ('django', 'Django'),
-    ('generica', 'Generica'),
+        ('linux', 'Linux'),
+        ('web', 'Web'),
+        ('django', 'Django'),
+        ('generica', 'Generica'),
     )
-    linux='Linux'
-    web='Web'
-    django='Django'
-    generica='Generica'
+    linux = 'Linux'
+    web = 'Web'
+    django = 'Django'
+    generica = 'Generica'
     title = models.CharField(max_length=250)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     category = models.CharField(max_length=50,
-    choices=CATEGORY,
-    default=generica)
+                                choices=CATEGORY,
+                                default=generica)
+
     class Meta:
         ordering = ('-created',)
+
     def __str__(self):
         return "%s" % (self.title)
-
-
 
 
 class Tutorial(models.Model):
     STATUS_CHOICES = (
-    ('bozza', 'Bozza'),
-    ('pubblicato', 'Pubblicato'),)
+        ('bozza', 'Bozza'),
+        ('pubblicato', 'Pubblicato'),)
     #post=models.ForeignKey(Comment,related_name='comments',on_delete=models.CASCADE,null=True,blank=True)
     title = models.CharField(max_length=250)
     overview = models.TextField(default="tutorial")
-    slug = models.SlugField(max_length=250,unique_for_date='publish',null=True,blank=True)
-    author = models.ForeignKey(Profile,on_delete=models.CASCADE,blank=True,null=True)
+    slug = models.SlugField(max_length=250, null=True, blank=True)
+    author = models.ForeignKey(
+        Profile, on_delete=models.CASCADE, blank=True, null=True)
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    category=models.ForeignKey(Category,on_delete=models.CASCADE,related_name="tutorials")
-    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='bozza')
-    #post=models.ForeignKey(Comment,on_delete=models.CASCADE,related_name="comments",null=True,blank=True)
-        # decommentare una delle seguenti tre righe per selezionare un custom model manager
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="tutorials")
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='bozza')
+#post=models.ForeignKey(Comment,on_delete=models.CASCADE,related_name="comments",null=True,blank=True)
+    # decommentare una delle seguenti tre righe per selezionare un custom model manager
     #objects = models.Manager() # The default manager.
     #bozza=BozzaManager() # Our bozza solo manager
     #published = PublishedManager() # Our publicato custom manager.
 
-
     class Meta:
         ordering = ('-publish',)
+
     def __str__(self):
         return "%s" % (self.title)
+
     def get_absolute_url(self):
         return reverse('homepage:tutorial_detail',
-        args=[self.publish.year,
-        self.publish.month,
-        self.publish.day,
-        self.slug])
+                       args=[self.publish.year,
+                             self.publish.month,
+                             self.publish.day,
+                             self.slug])
 
 
-
+@receiver(post_save, sender=Tutorial)
+def update_tutorial(sender, instance, **kwargs):
+    instance.slug = instance.title
+    print(str(instance.slug))
+    post_save.disconnect(update_tutorial, sender=Tutorial)
+    instance.save(update_fields=['slug'])
+    post_save.connect(update_tutorial, sender=Tutorial)
 
 # la classe category definisce le categorie in cui sarannno inseriti i singoli tutorials
 # ad esempio sara creata la categoria linux, web , ecc..
