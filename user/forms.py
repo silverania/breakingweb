fa35d2b90c3209a702512ff32da09b345294da29
silverania <spaceissimo@gmail.com>
2022-06-12 19:56:44 +0200

    aggiuntcampPhotouformIregistrazion

--- user/forms.py
+++ user/forms.py
@@ -12,7 +12,8 @@ class LoginForm(forms.Form):
 
 
 class SignUpForm(UserCreationForm):
-    photo = forms.CharField()
+    photo = forms.CharField(
+        max_length=300, required=False, help_text='Optional.')
 
     class Meta:
         model = User
