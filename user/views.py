cea8d7b3451c3922b3478a4fbcd35de6607cacb0
silverania <spaceissimo@gmail.com>
2022-06-23 23:01:28 +0200

    aggcustomizzatUlogin

--- user/views.py
+++ user/views.py
@@ -5,11 +5,29 @@ from .models import Profile
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
-from .forms import SignUpForm
+from .forms import SignUpForm, LoginForm
+from django.http import HttpResponse, HttpResponseRedirect
 
 
-def login(request, user):
-    return render(request, 'login_success.html')
+def user_login(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            myuser = authenticate(request,
+                                  username=cd['username'],
+                                  password=cd['password'])
+            if myuser is not None:
+                if myuser.is_active:
+                    login(request, myuser)
+                    return HttpResponseRedirect("/")
+                else:
+                    return HttpResponse('Disabled account')
+            else:
+                return HttpResponse('Invalid login')
+    else:
+        form = LoginForm()
+    return render(request, 'registration/login.html', {'form': form})
 
 
 @login_required
